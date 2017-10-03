3a89ae244b3ceaa16b7fbfdc1c40e7591d1c1f2c
raivonigul <raivonigul@gmail.com>
2017-10-03 22:21:28 +0300

    commit2

---
+++ Blink1/Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	10/3/2017 10:10:33 PM
+ Author:	raivo
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
